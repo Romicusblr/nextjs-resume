cb6072d1e312a02cc0c69be4cf06c20697578d6b
Raman Mastyka <raman.mastyka@gmail.com>
2025-05-15 00:27:18 +0200

    feat: update links, remove unused links

--- edit-me/config/links.ts
+++ edit-me/config/links.ts
@@ -1,36 +1,18 @@
 import { CMSLink } from '@config-types/cms-link';
 import {
-  SiBluesky,
   SiGithub,
-  SiInstagram,
   SiLinkedin,
-  SiNpm,
 } from '@icons-pack/react-simple-icons';
 
 export const links: CMSLink[] = [
   {
-    href: 'https://www.linkedin.com',
+    href: 'https://www.linkedin.com/in/raman-mastyka/',
     icon: SiLinkedin,
     title: 'LinkedIn',
   },
   {
-    href: 'https://github.com',
+    href: 'https://github.com/Romicusblr',
     icon: SiGithub,
     title: 'GitHub',
-  },
-  {
-    href: 'https://www.npmjs.com',
-    icon: SiNpm,
-    title: 'NPM',
-  },
-  {
-    href: 'https://bsky.app',
-    icon: SiBluesky,
-    title: 'Bluesky',
-  },
-  {
-    href: 'https://www.instagram.com',
-    icon: SiInstagram,
-    title: 'Instagram',
   },
 ];
